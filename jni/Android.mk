6131fe527ba379b9e26b0ed16850ffabbc86265a
young <young.mwy@gmail.com>
2014-11-27 11:07:02 +0800

    1. enable downcall activity 2. add some ant intermediate dirs and files into .gitignore

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := eng
-LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libdowncall
 LOCAL_SRC_FILES := downcall.c
 LOCAL_LDFLAGS := -llog
--- jni/downcall.c
+++ jni/downcall.c
@@ -3,7 +3,7 @@
 #include <jni.h>
 #include <android/log.h>
 
-jstring Java_com_young_jniinterface_DownloadActivity_downcall(JNIEnv *env, jobject obj)
+jstring Java_com_young_jniinterface_DowncallActivity_downcall(JNIEnv *env, jobject obj)
 {
     LOGI("downcall ...");
     return (*env)->NewStringUTF(env, "Here is in downcall");
