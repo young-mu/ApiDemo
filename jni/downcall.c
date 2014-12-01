c36c9c6e3d1b0a6e55ca2a8749d92cd1fa079f13
young <young.mwy@gmail.com>
2014-12-01 20:36:24 +0800

    remove dlerror judgement after dlsym

--- jni/downcall.c
+++ jni/downcall.c
@@ -15,19 +15,18 @@ jstring Java_com_young_jniinterface_DowncallActivity_downcallMtd2(JNIEnv *env, j
     void (*callfunc)();
     const char *err = NULL;
 
-    handle = dlopen("/data/data/com.young.jniinterface/lib/libcallee.so", RTLD_LAZY);
+    handle = dlopen("/data/data/com.young.jniinterface/lib/libcallee.so", RTLD_NOW);
     if (!handle) {
         LOGI("dlopen failed! (%s)", dlerror());
         return (*env)->NewStringUTF(env, "Error: dlopen");
     }
 
     callfunc = dlsym(handle, "callee");
-    if ((err = dlerror()) != NULL) {
-        LOGI("dlsym failed! (%s)", err);
-        return (*env)->NewStringUTF(env, "Error: dlsym");
+    // NOTE: dlerror returns 'Symbol not found' though dlsym returns valid address
+    if (callfunc != NULL) {
+        callfunc();
     }
 
-    callfunc();
     dlclose(handle);
 
     return (*env)->NewStringUTF(env, "Here is in downcall method 2");
