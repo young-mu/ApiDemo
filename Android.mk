0fac9e7365c1571a1f6d460829ced746bcc1c275
young <young.mwy@gmail.com>
2014-12-24 14:19:00 +0800

    delete Android.mk which is not used in ant build project

--- Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := eng
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PACKAGE_NAME := JNIitf
-
-include $(BUILD_PACKAGE)
