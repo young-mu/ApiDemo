6a6f8698becd2d9255903ccaf34eef3d2731746d
young <young.mwy@gmail.com>
2015-06-03 12:53:43 +0800

    add -g -O0 option

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,24 +3,28 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := callee
 LOCAL_SRC_FILES := callee.c
+LOCAL_CFLAGS := -g -O0
 LOCAL_LDFLAGS := -llog
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := downcall
 LOCAL_SRC_FILES := downcall.c
+LOCAL_CFLAGS := -g -O0
 LOCAL_LDFLAGS := -llog -ldl
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := downcall_onload
 LOCAL_SRC_FILES := downcall_onload.c
+LOCAL_CFLAGS := -g -O0
 LOCAL_LDFLAGS := -llog -ldl
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := upcall
 LOCAL_SRC_FILES := upcall.c
+LOCAL_CFLAGS := -g -O0
 LOCAL_LDFLAGS := -llog
 include $(BUILD_SHARED_LIBRARY)
 
