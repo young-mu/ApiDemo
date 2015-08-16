5bc3d3538aa98d75a265ed1f1f65e7cb3d6a4d2e
young <young.mwy@gmail.com>
2015-08-16 20:42:29 +0800

    add signal method1 implementation

--- jni/signal.c
+++ jni/signal.c
@@ -1,11 +1,32 @@
 #include "_log.h"
-#include <string.h>
 #include <jni.h>
-#include <dlfcn.h>
-#include <stdio.h>
-#include <fcntl.h>
+#include <signal.h>
+#include <assert.h>
+
+void sigHandler1(int signo) {
+    switch (signo) {
+        case SIGUSR1:
+            LOGI("catch a signal - SIGUSR1\n");
+            break;
+        case SIGUSR2:
+            LOGI("catch a signal - SIGUSR2\n");
+            break;
+        default:
+            LOGI("wrong signo!\n");
+            break;
+    }
+}
 
 jstring Java_com_young_jniinterface_SignalActivity_downcallMtd1(JNIEnv *env, jobject obj) {
     LOGI("trigger downcall! (%s)", __func__);
-    return (*env)->NewStringUTF(env, "Here is in signal method 1");
+
+    signal(SIGUSR1, sigHandler1);
+    signal(SIGUSR2, sigHandler1);
+
+    LOGI("raise SIGUSR1");
+    raise(SIGUSR1);
+    LOGI("raise SIGUSR2");
+    raise(SIGUSR2);
+
+    return (*env)->NewStringUTF(env, "Here is in signal method using raise");
 }
