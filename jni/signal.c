665b1a48f6af137c3b41ff87e263d8812cbf63a3
young <young.mwy@gmail.com>
2015-08-28 15:32:38 +0800

    add SIGSEGV test

--- jni/signal.c
+++ jni/signal.c
@@ -6,9 +6,6 @@
 
 void sigHandler1(int signo) {
     switch (signo) {
-        case SIGPIPE:
-            LOGI("catch a signal - SIGPIPE");
-            break;
         case SIGUSR2:
             LOGI("catch a signal - SIGUSR2");
             break;
@@ -31,7 +28,48 @@ jstring Java_com_young_jniinterface_SignalActivity_SignalTest2(JNIEnv *env, jobj
     return (*env)->NewStringUTF(env, "send SIGUSR2 to self");
 }
 
-void *thrHandler3(void *arg)
+void sigHandler3(int signo) {
+    switch (signo) {
+        case SIGSEGV:
+            LOGI("catch a signal - SIGUSR2");
+            break;
+        default:
+            LOGI("wrong signo!");
+            break;
+    }
+}
+
+jstring Java_com_young_jniinterface_SignalActivity_SignalTest3(JNIEnv *env, jobject obj) {
+    struct sigaction sa;
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = sigHandler3;
+    sigaction(SIGUSER1, &sa, NULL);
+    LOGI("register signal SIGSEGV (pid = %d)", getpid());
+    return (*env)->NewStringUTF(env, "register signal SIGSEGV");
+}
+
+jstring Java_com_young_jniinterface_SignalActivity_SignalTest4(JNIEnv *env, jobject obj) {
+    int pid = getpid();
+    LOGI("send SIGSEGV to self process (pid = %d)", getpid());
+    tkill(pid, SIGSEGV);
+    return (*env)->NewStringUTF(env, "send SIGSEGV to self");
+}
+
+void sigHandler5(int signo) {
+    switch (signo) {
+        case SIGPIPE:
+            LOGI("catch a signal - SIGPIPE");
+            break;
+        case SIGUSR2:
+            LOGI("catch a signal - SIGUSR2");
+            break;
+        default:
+            LOGI("wrong signo!");
+            break;
+    }
+}
+
+void *thrHandler5(void *arg)
 {
     sigset_t set;
     sigemptyset(&set);
@@ -41,12 +79,12 @@ void *thrHandler3(void *arg)
     LOGI("[child %d] go on, sigsuspend returns %d", gettid(), ret);
 }
 
-jstring Java_com_young_jniinterface_SignalActivity_SignalTest3(JNIEnv *env, jobject obj) {
+jstring Java_com_young_jniinterface_SignalActivity_SignalTest5(JNIEnv *env, jobject obj) {
     // must register SIGPIPE, or else will not execute pending SIGUSR2 sigHandler
-    signal(SIGPIPE, sigHandler1);
-    signal(SIGUSR2, sigHandler1);
+    signal(SIGPIPE, sigHandler5);
+    signal(SIGUSR2, sigHandler5);
     pthread_t thr;
-    int err = pthread_create(&thr, NULL, thrHandler3, NULL);
+    int err = pthread_create(&thr, NULL, thrHandler5, NULL);
     if (err != 0) {
         LOGI("pthread_create failed (%s)", strerror(err));
         return (*env)->NewStringUTF(env, "Error: pthread_create");
--- src/com/young/jniinterface/SignalActivity.java
+++ src/com/young/jniinterface/SignalActivity.java
@@ -13,6 +13,8 @@ public class SignalActivity extends Activity implements OnClickListener {
     private Button methodBtn1;
     private Button methodBtn2;
     private Button methodBtn3;
+    private Button methodBtn4;
+    private Button methodBtn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,20 @@ public class SignalActivity extends Activity implements OnClickListener {
         methodBtn1 = (Button)findViewById(R.id.sig_method_button1);
         methodBtn2 = (Button)findViewById(R.id.sig_method_button2);
         methodBtn3 = (Button)findViewById(R.id.sig_method_button3);
+        methodBtn4 = (Button)findViewById(R.id.sig_method_button4);
+        methodBtn5 = (Button)findViewById(R.id.sig_method_button5);
         methodBtn1.setOnClickListener(this);
         methodBtn2.setOnClickListener(this);
         methodBtn3.setOnClickListener(this);
+        methodBtn4.setOnClickListener(this);
+        methodBtn5.setOnClickListener(this);
     }
 
     public native String SignalTest1();
     public native String SignalTest2();
     public native String SignalTest3();
+    public native String SignalTest4();
+    public native String SignalTest5();
 
     static {
         System.loadLibrary("signal");
@@ -48,6 +56,12 @@ public class SignalActivity extends Activity implements OnClickListener {
             break;
         case R.id.sig_method_button3:
             SignalTxt.setText(SignalTest3());
+            break;
+        case R.id.sig_method_button4:
+            SignalTxt.setText(SignalTest4());
+            break;
+        case R.id.sig_method_button5:
+            SignalTxt.setText(SignalTest5());
             break;
         default:
             break;
